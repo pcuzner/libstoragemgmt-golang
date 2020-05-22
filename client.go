eff2a6ece9b370afde32486fe2d6e50139cfdaaf
Tony Asleson <tasleson@redhat.com>
2020-05-22 16:58:43 -0500

    Add VolumeDisable call
    Signed-off-by: Tony Asleson <tasleson@redhat.com>

--- client.go
+++ client.go
@@ -498,6 +498,13 @@ func (c *ClientConnection) VolumeEnable(vol *Volume) error {
 	return c.tp.invoke("volume_enable", args, nil)
 }
 
+// VolumeDisable sets a volume to offline.
+func (c *ClientConnection) VolumeDisable(vol *Volume) error {
+	var args = make(map[string]interface{})
+	args["volume"] = *vol
+	return c.tp.invoke("volume_disable", args, nil)
+}
+
 // VolumeMask grants access to a volume for the specified access group.
 func (c *ClientConnection) VolumeMask(vol *Volume, ag *AccessGroup) error {
 	var args = make(map[string]interface{})
