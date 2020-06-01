d7cfc99ee88df3f7a84f135c7321e50b31d542c5
Tony Asleson <tasleson@redhat.com>
2020-06-01 15:39:01 -0500

    cov.sh: Check for existence of LSMSRC
    Signed-off-by: Tony Asleson <tasleson@redhat.com>

--- test/cov.sh
+++ test/cov.sh
@@ -1,8 +1,13 @@
 #/bin/bash
 
-export CGO_CFLAGS="-I$LSMSRC/c_binding/include"
-export CGO_LDFLAGS="-L$LSMSRC/c_binding/.libs/ -lstoragemgmt"
-export LD_LIBRARY_PATH="$LSMSRC/c_binding/.libs/"
+if [[ -z "${LSMSRC}" ]]; then
+    export CGO_LDFLAGS="-lstoragemgmt"
+else
+    export CGO_CFLAGS="-I$LSMSRC/c_binding/include"
+    export CGO_LDFLAGS="-L$LSMSRC/c_binding/.libs/ -lstoragemgmt"
+    export LD_LIBRARY_PATH="$LSMSRC/c_binding/.libs/"
+fi
+
 export LSM_GO_URI="simc://"
 
 go test -count 1 github.com/libstorage/libstoragemgmt-golang/test -coverpkg=../. -cover -coverprofile client.out || exit 1
