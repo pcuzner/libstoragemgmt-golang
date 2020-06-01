ea0a754a64ca7164fcf1987fbe178ecf851a618e
Tony Asleson <tasleson@redhat.com>
2020-06-01 10:17:25 -0500

    Expand cov.sh
    Add things needed to build with native code.
    
    Signed-off-by: Tony Asleson <tasleson@redhat.com>

--- test/cov.sh
+++ test/cov.sh
@@ -1,5 +1,13 @@
 #/bin/bash
 
-LSM_GO_URI=simc:// go test -count 1 github.com/libstorage/libstoragemgmt-golang/test -coverpkg=../. -cover -coverprofile cc.out || exit 1
+export CGO_CFLAGS="-I$LSMSRC/c_binding/include"
+export CGO_LDFLAGS="-L$LSMSRC/c_binding/.libs/ -lstoragemgmt"
+export LD_LIBRARY_PATH="$LSMSRC/c_binding/.libs/"
+export LSM_GO_URI="simc://"
+
+go test -count 1 github.com/libstorage/libstoragemgmt-golang/test -coverpkg=../. -cover -coverprofile client.out || exit 1
+go test -count 1 github.com/libstorage/libstoragemgmt-golang/test -coverpkg=.././localdisk -cover -coverprofile localdisk.out || exit 1
+
+go tool cover -html=client.out || exit 1
 go tool cover -html=cc.out || exit 1
 
