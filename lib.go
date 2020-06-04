ef41532bc6a1262cd9904a24f2a10cd90ece80c6
Tony Asleson <tasleson@redhat.com>
2020-06-04 17:25:10 -0500

    handleSearch: Use switch statement
    Signed-off-by: Tony Asleson <tasleson@redhat.com>

--- lib.go
+++ lib.go
@@ -34,15 +34,16 @@ func emptySliceIfNil(provided []string) []string {
 }
 
 func handleSearch(args map[string]interface{}, search []string) bool {
-	var rc = true
+	rc := true
 
-	if len(search) == 0 {
+	switch num := len(search); num {
+	case 0:
 		args["search_key"] = nil
 		args["search_value"] = nil
-	} else if len(search) == 2 {
+	case 2:
 		args["search_key"] = search[0]
 		args["search_value"] = search[1]
-	} else {
+	default:
 		rc = false
 	}
 	return rc
