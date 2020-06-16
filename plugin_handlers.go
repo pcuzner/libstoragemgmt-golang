aa4a35b6d7a28a1ae0d70358f53cbb1dc66bc0b8
Tony Asleson <tasleson@redhat.com>
2020-06-16 16:12:42 -0500

    Add fs delete table entry
    Signed-off-by: Tony Asleson <tasleson@redhat.com>

--- plugin_handlers.go
+++ plugin_handlers.go
@@ -669,6 +669,7 @@ func buildTable(c *PluginCallBacks) map[string]handler {
 
 		"fs":                     nilAssign(c.File.FileSystems, handleFs),
 		"fs_create":              nilAssign(c.File.FsCreate, handleFsCreate),
+		"fs_delete":              nilAssign(c.File.FsDelete, handleFsDelete),
 		"fs_child_dependency":    nilAssign(c.File.FsHasChildDep, handleFsHasChildDep),
 		"fs_child_dependency_rm": nilAssign(c.File.FsChildDepRm, handleFsChildDepRm),
 	}
