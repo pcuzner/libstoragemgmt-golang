cbbdf122a26f7feaca62521be75ce6b7d5f37071
Tony Asleson <tasleson@redhat.com>
2020-06-16 16:14:40 -0500

    Add fs snapshot table entry
    Signed-off-by: Tony Asleson <tasleson@redhat.com>

--- plugin_handlers.go
+++ plugin_handlers.go
@@ -675,6 +675,7 @@ func buildTable(c *PluginCallBacks) map[string]handler {
 		"fs_file_clone":          nilAssign(c.File.FsFileClone, handleFsFileClone),
 		"fs_snapshot_create":     nilAssign(c.File.FsSnapShotCreate, handleFsSnapShotCreate),
 		"fs_snapshot_delete":     nilAssign(c.File.FsSnapShotDelete, handleFsSnapShotDelete),
+		"fs_snapshots":           nilAssign(c.File.FsSnapShots, handleFsSnapShots),
 		"fs_child_dependency":    nilAssign(c.File.FsHasChildDep, handleFsHasChildDep),
 		"fs_child_dependency_rm": nilAssign(c.File.FsChildDepRm, handleFsChildDepRm),
 	}
